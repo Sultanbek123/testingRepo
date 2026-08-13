090c71c0b38d9684c60dc92e510d68fcda5a5156
Sultanbek <skenzhahimov@gmail.com>
2026-08-13 23:47:18 +0500

    laungh two goroutines

--- main/main.go
+++ main/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func firstAction(ch chan string) {
+	ch <- "message"
+	fmt.Println("Message has been sent")
+}
+func secondAction(ch chan string) {
+	time.Sleep(5 * time.Second)
+	message := <-ch
+	fmt.Println("received message :", message)
+}
 
 func main() {
 	var message string = "Hi my name is Sultanbek"
 	fmt.Println(message)
+	ch := make(chan string)
+	go firstAction(ch)
+	go secondAction(ch)
+	time.Sleep(10 * time.Second)
 }
